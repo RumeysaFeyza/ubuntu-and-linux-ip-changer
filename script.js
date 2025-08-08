f80831a165273642658180d62b9d55d30e03b976
feyza <116541759+213405067@users.noreply.github.com>
2025-08-09 01:10:38 +0300

    degisiklikler

--- script.js
+++ script.js
@@ -5,4 +5,17 @@ function goToSetupLAN() {
 function setupAcsess() {
   window.location.href = "setup-acsess.html";
 }
+function goToNameServers() {
+  window.location.href = "setup-name-servers.html";
+}
+
+function goToIPv4Routes() {
+  window.location.href = "setup-ipv4-routes.html";
+}
+function goToIPv6Routes() {
+  window.location.href = "setup-ipv6-routes.html";
+}
+function goToChangeHostname() {
+  window.location.href = "change-hostname.html";
+}
 
